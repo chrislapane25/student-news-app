65939b06fc29eb1b7fc70c756ccd4072fce28bdd
chrislapane25 <chrslapane25@gmail.com>
2026-02-10 22:07:53 +0200

    Initial Flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Student News App is running 🚀"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
